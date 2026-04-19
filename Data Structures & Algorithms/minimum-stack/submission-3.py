0b34475b0fbced749569def2e5f6d3184ebb1b2f
Purshottam Kumar <155253317+PurshottamKumar07@users.noreply.github.com>
2026-04-19 13:24:20 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.temp=(float('inf'))
+        self.lenn=0
+
+    def push(self, val: int) -> None:
+        if val<=self.temp:
+            self.temp=val
+        self.stack.append((val,self.temp))
+        self.lenn+=1
+
+    def pop(self) -> None:
+        self.stack.pop()
+        if self.lenn>1:
+            self.temp=self.stack[-1][1]
+            self.lenn-=1
+        else:
+            self.lenn-=1
+            self.temp=float('inf')
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
