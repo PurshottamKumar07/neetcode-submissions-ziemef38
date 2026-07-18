9d272d2738e9ebf62ad78aa0dadd459ee3580b55
Purshottam Kumar <155253317+PurshottamKumar07@users.noreply.github.com>
2026-07-19 02:31:53 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums)==1:
+            return nums[0]
+        
+        def rec(n,memo):
+            if n>=len(nums):
+                return 0
+
+            if n in memo:
+                return memo[n]
+            
+            else:
+                memo[n]=max(rec(n+2,memo)+nums[n],
+                            rec(n+1,memo))
+                
+                return memo[n]
+        
+        memo={}
+    
+        return max(rec(0,memo),rec(1,memo))
